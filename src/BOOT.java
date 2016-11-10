71a4c976cced62f5643ce0d66c7779aba8393704
jacob <robertsjw2@vcu.edu>
2016-11-10 15:18:53 -0500

    uncreative commit message

---
+++ src/BOOT.java
@@ -0,0 +1,13 @@
+/*
+ * main java class, initializes operating system simulation
+ * 
+ * created by Jrobertzz on 11/10/2016
+ */
+public class BOOT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/CPU.java
@@ -0,0 +1,15 @@
+/*
+ * CPU class
+ * one loop is one "tick" or cycle
+ * will need to add basic commands
+ * I think(mov mult etc.)
+ * 
+ * Created by Jrobertzz 11/10/2016
+ */
+public class CPU {
+		public void tick(){
+			while(1==1){
+				
+			}
+		}
+}
