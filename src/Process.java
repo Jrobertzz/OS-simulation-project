b7a75acf38e53e354e7427e1e5528f1ad3d386ae
MATT0568 <lesniewiczm@vcu.edu>
2016-11-30 21:39:35 -0500

    Removed some print statements used for testing

--- src/Process.java
+++ src/Process.java
@@ -5,27 +5,9 @@ public class Process {
 	RAMFrames frames;
 	
 	public Process(Memory t, RAMFrames f){
-		//System.out.println();
-		//System.out.println("1");
 		frames = f;
 		RAM = t;
-		Interpretor byteInterpretor = new Interpretor();
-		this.PID = PIDGenerator.newPID();
-		
-		//System.out.println("2");
-		
-		// TESTINT
-		reqMem = 50;
-		System.out.println("3");
-		setLocalMem();
-		System.out.println("the first 4");
-		byte[] test = byteInterpretor.stringToByteArray("THIS IS A TEST");
-		System.out.println("test:::::  " + byteInterpretor.byteArrayToString(test));
-		System.out.println("test:::::  " + localMem[25]);
-		localMem[25].setFrame(test);
-		System.out.println("test");
-		System.out.println(byteInterpretor.byteArrayToString(localMem[25].getFrame()));
-		
+		this.PID = PIDGenerator.newPID();		
 	}
 	
 	public int getPID(){
@@ -41,10 +23,7 @@ public class Process {
 	}
 	
 	public void setLocalMem(){
-		System.out.println("the 4");
-		System.out.println("5");
 		localMem = frames.allocateFrames(reqMem);
-		System.out.println("the 6");
 	}
 	
 	
